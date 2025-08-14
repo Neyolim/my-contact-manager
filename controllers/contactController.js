38bf06ffcb2895192567d83695e1a266fb7206d3
Neyolim <neyoollim@gmail.com>
2025-08-14 11:23:27 +0545

    add CRUD in Routes

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,53 +1,88 @@
+const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
 //@desc Get all contacts
 //@route GET /api/contacts
-//@access public 
+//@access public
 
-const getContacts = (req, res) => {
-  res.status(200).json({ message: "Get all contacts" });
-};
+const getContacts = asyncHandler(async (req, res) => {
+  const contacts = await Contact.find();
+  res.status(200).json(contacts);
+});
 
 //@desc Create New contact
 //@route POST /api/contacts
 //@access public
 
-const createContact = (req, res) => {
-  console.log("The request body: ",req.body);
+const createContact = asyncHandler(async (req, res) => {
+  console.log("The request body: ", req.body);
   const { name, email, phone } = req.body;
-  if (!name || !email || !phone){
+  if (!name || !email || !phone) {
     res.status(400);
     throw new Error("All the fields are necessary");
   }
-  res.status(201).json({ message: "Create contacts" });
-}
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(201).json(contact);
+});
 
 //@desc Get contact
 //@route POST /api/contacts/:id
 //@access public
 
-const getContact = (req, res) => {
-  res.status(200).json({ message: `Get contact for ${req.params.id}` });
-}
+const getContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+
+  res.status(200).json(contact);
+});
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
 //@access public
 
-const updateContact = (req, res) => {
-  res.status(200).json({ message: `Update contact for ${req.params.id}` });
-}
+const updateContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.status(200).json(updatedContact);
+});
 
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
 //@access public
 
-const deleteContact = (req,res) => {
-    res.status(200).json({message:`Delete contact for ${req.params.id}`})
-}
+const deleteContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+ await Contact.findByIdAndDelete(req.params.id);
+  res.status(200).json(contact);
+});
 
-
-module.exports = {getContacts, createContact, getContact, updateContact, deleteContact};
-
-
+module.exports = {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+};
 
 //  In backend development, especially when using the MVC pattern (Model-View-Controller), a controller is a piece of code that:
 
